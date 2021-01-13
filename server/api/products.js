b51e0fc03064edb12f55c5ecac4a39739c23e324
Priscila <priscilapintado66@gmail.com>
2021-01-13 08:46:56 -0500

    feat: API route for single product created

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Product} = require('../db/models')
+const {Product, Cart} = require('../db/models')
 module.exports = router
 
 //GET  /api/products
@@ -7,6 +7,18 @@ router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll()
     res.send(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//GET /api/products/:productId
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId, {
+      include: Cart
+    })
+    res.send(product)
   } catch (err) {
     next(err)
   }

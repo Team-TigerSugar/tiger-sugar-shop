4eda9093a1c309932037ea3d19d098b87e108f44
Julia Kravets <juliaakravets@gmail.com>
2021-01-15 21:22:16 -0500

    added travis key

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
+require('../../secrets')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
@@ -7,6 +8,15 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false
+    //  operatorAliases: false,
+    //  dialect: 'postgres',
+    //  protocol: 'postgres',
+    //  ssl: true,
+    //  dialectOptions: {
+    //    ssl: process.env.DATABASE_URL && {
+    //      require: true,
+    //    },
+    //  },
   }
 )
 module.exports = db

c70351a756989619aa12d10f819e1b6841b3becd
Priscila <priscilapintado66@gmail.com>
2021-01-17 15:33:17 -0500

    pushed to test

--- client/components/updateCart.js
+++ client/components/updateCart.js
@@ -61,7 +61,7 @@ class UpdateCart extends Component {
 
   async handleSubmit(event) {
     event.preventDefault()
-    console.log(this.state)
+    console.log('tttttyyy', this.state)
     const userId = this.props.user.id
     const itemId = this.props.item.id
     const qty = this.state.qty

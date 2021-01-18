8b8b16c80d1a06a4b21b6d57fcaca746ff962b35
Lindsey Pak <paklindsey@gmail.com>
2021-01-17 21:59:55 -0700

    UserHome.js styling

--- client/components/UserHome.js
+++ client/components/UserHome.js
@@ -1,31 +1,86 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
+import {compose} from 'redux'
+import {auth} from '../store'
+import {withStyles} from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
 
-/**
- * COMPONENT
- */
+import Footer from './Footer'
+
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+import info from '../../public/images/info.png'
+
+const styles = theme => ({
+  headerText: {
+    marginTop: '2em',
+    width: '10em'
+  },
+  image: {
+    width: '28em'
+  },
+  button: {
+    backgroundColor: theme.palette.common.colorTwo,
+    marginTop: '4em'
+  }
+})
+
 export const UserHome = props => {
-  const {firstName, lastName, email, shippingInfo, billingInfo} = props
+  const {firstName, classes, lastName, email, shippingInfo, billingInfo} = props
 
   return (
-    <div>
-      <h2>Welcome, {firstName}</h2>
-      <h3>Account Information:</h3>
-      <h4>
-        name: {firstName} {lastName}
-      </h4>
-      <h4>email: {email}</h4>
-      <h3>Address Book</h3>
-      <h4>shipping address: {shippingInfo}</h4>
-      <h4>billing address: {billingInfo}</h4>
-    </div>
+    <Grid container direction="column">
+      <Grid container direction="row" justify="center">
+        <Typography
+          variant="h1"
+          justify="center"
+          className={classes.headerText}
+        >
+          Welcome, {firstName}
+        </Typography>
+      </Grid>
+      <Grid container direction="row" style={{marginTop: '5em'}}>
+        <Grid item container lg style={{width: '50%'}} justify="flex-end">
+          <img src={info} alt="flower by window" className={classes.image} />
+        </Grid>
+        <Grid
+          item
+          container
+          lg
+          style={{width: '50%', marginTop: '5em', paddingLeft: '7em'}}
+        >
+          <Grid item>
+            <Typography variant="body1" style={{marginBottom: '0.7em'}}>
+              NAME:
+            </Typography>
+            <Typography variant="body2" style={{marginBottom: '2em'}}>
+              {firstName} {lastName}
+            </Typography>
+            <Typography variant="body1" style={{marginBottom: '0.7em'}}>
+              EMAIL:
+            </Typography>
+            <Typography variant="body2" style={{marginBottom: '2em'}}>
+              {email}
+            </Typography>
+            <Typography variant="body1" style={{marginBottom: '0.7em'}}>
+              ADDRESSES:
+            </Typography>
+            <Typography variant="body2">{shippingInfo}</Typography>
+            <Button variant="filled" className={classes.button}>
+              Edit
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Footer />
+    </Grid>
   )
 }
 
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     firstName: state.user.firstName,
@@ -36,11 +91,17 @@ const mapState = state => {
   }
 }
 
-export default connect(mapState)(UserHome)
+const mapDispatch = dispatch => {
+  return {
+    auth: (email, password, method) => dispatch(auth(email, password, method))
+  }
+}
 
-/**
- * PROP TYPES
- */
+export default compose(
+  connect(mapState),
+  withStyles(styles, {withTheme: true})
+)(UserHome)
+
 UserHome.propTypes = {
   firstName: PropTypes.string,
   lastName: PropTypes.string,
@@ -48,3 +109,55 @@ UserHome.propTypes = {
   shippingInfo: PropTypes.string,
   billingInfo: PropTypes.string
 }
+
+// ============================================================
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import {connect} from 'react-redux'
+
+// /**
+//  * COMPONENT
+//  */
+// export const UserHome = props => {
+//   const {firstName, lastName, email, shippingInfo, billingInfo} = props
+
+//   return (
+//     <div>
+//       <h2>Welcome, {firstName}</h2>
+//       <h3>Account Information:</h3>
+//       <h4>
+//         name: {firstName} {lastName}
+//       </h4>
+//       <h4>email: {email}</h4>
+//       <h3>Address Book</h3>
+//       <h4>shipping address: {shippingInfo}</h4>
+//       <h4>billing address: {billingInfo}</h4>
+//     </div>
+//   )
+// }
+
+// /**
+//  * CONTAINER
+//  */
+// const mapState = state => {
+//   return {
+//     firstName: state.user.firstName,
+//     lastName: state.user.lastName,
+//     email: state.user.email,
+//     shippingInfo: state.user.shippingInfo,
+//     billingInfo: state.user.billingInfo
+//   }
+// }
+
+// export default connect(mapState)(UserHome)
+
+// /**
+//  * PROP TYPES
+//  */
+// UserHome.propTypes = {
+//   firstName: PropTypes.string,
+//   lastName: PropTypes.string,
+//   email: PropTypes.string,
+//   shippingInfo: PropTypes.string,
+//   billingInfo: PropTypes.string
+// }
--- client/components/dummy.js
+++ client/components/dummy.js
@@ -1,202 +1,73 @@
-// import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-// import { connect } from "react-redux";
-// import { auth } from "../redux/user";
-// import OAuthForm from "./OAuthForm";
-// import Avatar from "avataaars";
-// class Login extends Component {
-//    constructor() {
-//       super();
-//       this.state = {
-//          email: “”,
-//          password: “”,
-//          //is this safe?
-//          method: "log-in",
-//          errorMessage: "",
-//       };
-//       this.handleSubmit = this.handleSubmit.bind(this);
-//       this.changeHandler = this.changeHandler.bind(this);
-//    }
-//    changeHandler(e) {
-//       this.setState({ [e.target.name]: e.target.value });
-//    }
-//    async handleSubmit(e) {
-//       e.preventDefault();
-//       const email = this.state.email;
-//       const password = this.state.password;
-//       const method = this.state.method;
-//       try {
-//          await this.props.auth(email, password, method);
-//       } catch (error) {
-//          console.log(error);
-//       }
-//    }
-//    render() {
-//       const error = this.props.error;
-//       const avatar = {
-//          width: “200px”,
-//          height: “200px”,
-//          alignSelf: “center”
-//       };
-//       return (
-//          <div className="main">
-//             <div className="login-container">
-//                <Avatar
-//                   style={avatar}
-//                   avatarStyle="Circle"
-//                   topType="LongHairCurly"
-//                   accessoriesType="Blank"
-//                   hairColor="PastelPink"
-//                   facialHairType="Blank"
-//                   clotheType="ShirtCrewNeck"
-//                   clotheColor="Gray01"
-//                   eyeType="Wink"
-//                   eyebrowType="Default"
-//                   mouthType="Smile"
-//                   skinColor="Light"
-//                />
-//                <form>
-//                   <label htmlFor="email"></label>
-//                   <input
-//                      type="text"
-//                      placeholder="Enter Email"
-//                      name="email"
-//                      required
-//                      onChange={this.changeHandler}
-//                   ></input>
-//                   <label htmlFor="password"></label>
-//                   <input
-//                      type="password"
-//                      placeholder="Enter Password"
-//                      name="password"
-//                      required
-//                      onChange={this.changeHandler}
-//                   ></input>
-//                   <button
-//                      className="form-button"
-//                      type="submit"
-//                      onClick={this.handleSubmit}
-//                   >
-//                      Log In
-//                   </button>
-//                   <div> {this.state.errorMessage} </div>
-//                </form>
-//                <OAuthForm authMethod="Log In" />
-//                <div className="login-footer">
-//                   <Link to="/sign-up">Create an Account</Link>
-//                </div>
-//             </div>
-//          </div>
-//       );
-//    }
-// }
-// const mapDispatch = (dispatch) => {
-//    return {
-//       auth: (email, password, method) => dispatch(auth(email, password, method)),
-//    };
-// };
-// export default connect(null, mapDispatch)(Login);
-
 // import React from 'react'
 // import {Link} from 'react-router-dom'
 // import {connect} from 'react-redux'
 // import {compose} from 'redux'
-// import PropTypes from 'prop-types'
 // import {auth} from '../store'
 // import {withStyles} from '@material-ui/core/styles'
+// import PropTypes from 'prop-types'
+
+// import Footer from './Footer'
 
 // import Grid from '@material-ui/core/Grid'
 // import Typography from '@material-ui/core/Typography'
 // import TextField from '@material-ui/core/TextField'
+// import Button from '@material-ui/core/Button'
 
 // const styles = (theme) => ({
-//   toolbar: {
-//     paddingTop: '1em',
-//     paddingBottom: '1em',
+//   wrapper: {
+//     width: '40em',
 //   },
-//   toolbarMargin: {
-//     ...theme.mixins.toolbar,
-//     marginBottom: '2em',
+//   inputWrapper: {
+//     width: '50%',
+//     marginTop: '5em',
 //   },
-//   navbar: {
-//     backgroundColor: theme.palette.common.colorWhite,
+//   inputField: {
+//     backgroundColor: theme.palette.common.colorThree,
 //   },
-//   tabsCont: {
-//     marginLeft: '9em',
-//   },
-//   tab: {
-//     ...theme.typography.tab,
-//   },
-//   tabs2: {
-//     ...theme.typography.tab,
-//     marginLeft: '66em',
-//     [theme.breakpoints.down('md')]: {
-//       marginLeft: '20em',
-//       color: 'fff',
-//     },
-//   },
-//   cartButtonImg: {
-//     width: '4em',
+//   button: {
+//     fontFamily: 'Lato',
+//     fontWeight: 300,
 //   },
 // })
 
-// const AuthForm = (props) => {
-//   const {name, displayName, handleSubmit, error} = props
-//   const classes = this.props
+// export const UserHome = (props) => {
+//   const {firstName, lastName, email, shippingInfo, billingInfo} = props
 
 //   return (
-//     //   ------------------ entire page wrapper
 //     <Grid container direction="column">
-//       <Grid item>
-//         <Typography variant="h1">Sign In</Typography>
+//       <Grid container>
+//         <Typography variant="h1">Welcome, {firstName}</Typography>
 //       </Grid>
-//       {/* //   ------------------ text fields wrapper */}
-//       <Grid item container direction='column'>
-//           <TextField
-//             label="Email"
-//             id="name"
-//             value={name}
-//             fullWidth
-//             onChange={(event) => setName(event.target.value)}
-//           ></TextField>
-//       </Grid>
+//       <Grid item container></Grid>
 //     </Grid>
 //   )
 // }
 
-// const mapLogin = (state) => {
+// const mapState = (state) => {
 //   return {
-//     name: 'login',
-//     displayName: 'Login',
-//     error: state.user.error,
-//   }
-// }
-// const mapSignup = (state) => {
-//   return {
-//     name: 'signup',
-//     displayName: 'Sign Up',
-//     error: state.user.error,
+//     firstName: state.user.firstName,
+//     lastName: state.user.lastName,
+//     email: state.user.email,
+//     shippingInfo: state.user.shippingInfo,
+//     billingInfo: state.user.billingInfo,
 //   }
 // }
 
 // const mapDispatch = (dispatch) => {
 //   return {
-//     handleSubmit(evt) {
-//       evt.preventDefault()
-//       const formName = evt.target.name
-//       const email = evt.target.email.value
-//       const password = evt.target.password.value
-//       dispatch(auth(email, password, formName))
-//     },
+//     auth: (email, password, method) => dispatch(auth(email, password, method)),
 //   }
 // }
 
-// export const Login = compose(
-//   connect(mapLogin, mapDispatch),
+// export default compose(
+//   connect(mapState),
 //   withStyles(styles, {withTheme: true})
-// )(AuthForm)
+// )(UserHome)
 
-// export const Signup = compose(
-//   connect(mapSignup, mapDispatch),
-//   withStyles(styles, {withTheme: true})
-// )(AuthForm)
+// UserHome.propTypes = {
+//   firstName: PropTypes.string,
+//   lastName: PropTypes.string,
+//   email: PropTypes.string,
+//   shippingInfo: PropTypes.string,
+//   billingInfo: PropTypes.string,
+// }
